799a5ca9f33bb45640dcf1237f7d6ab7d1977cc3
R-A-S <r.a.s@live.ru>
2020-01-06 16:14:15 +0200

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-basics';
+  title = 'Dynamic title';
+
+  number = 42;
+
+  arr = [1, 2, 3];
+
+  obj = {
+    a: 2,
+  };
 }
