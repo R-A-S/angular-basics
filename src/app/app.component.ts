70a3f79022716894b13ba33b3131d32bb192ff7c
R-A-S <r.a.s@live.ru>
2020-01-07 14:58:43 +0200

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  toggle: any = false;
+  posts = [
+    {
+      title: 'Post 1',
+      author: 'Ivan',
+      comments: [
+        { title: 'comment 1', text: 'Lorem 1' },
+        { title: 'comment 2', text: 'Lorem 2' },
+        { title: 'comment 3', text: 'Lorem 3' },
+      ],
+    },
+    {
+      title: 'Post 2',
+      author: 'Misha',
+      comments: [
+        { title: 'comment 1', text: 'Lorem 1' },
+        { title: 'comment 2', text: 'Lorem 2' },
+        { title: 'comment 3', text: 'Lorem 3' },
+      ],
+    },
+  ];
 }
