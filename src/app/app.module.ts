2dc46fac5a2c05a123414e1eb04ae883f53d1a0f
R-A-S <r.a.s@live.ru>
2020-01-07 15:56:18 +0200

    COMPONENTS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,10 @@ import {NgModule} from '@angular/core';
 
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
-import {PostComponent} from './post/post.component';
-import {Post2Component} from './post2/post2.component';
-import {Post3Component} from './post3/post3.component';
-import {Post4Component} from './post4/post4.component';
-
+import { PostComponent } from './post/post.component';
+import { PostFormComponent } from './post-form/post-form.component';
 @NgModule({
-  declarations: [AppComponent, PostComponent, Post2Component, Post3Component, Post4Component],
+  declarations: [AppComponent, PostComponent, PostFormComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
