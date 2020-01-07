22f7455bfe2322219d58cb248bc642c92b6e5bb3
R-A-S <r.a.s@live.ru>
2020-01-07 16:16:44 +0200

    passing parameters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
+  posts: Array<Post> = [
+    { title: 'I need to learn Angular Components', text: 'And still learning...😁', id: 1 },
+    { title: 'Next block about', text: 'directives and something else', id: 2 },
+  ];
 }
