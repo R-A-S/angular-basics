b2dfaed31718ca552cffb54d66cc736c245e1703
R-A-S <r.a.s@live.ru>
2020-01-08 11:11:10 +0200

    DIRECTIVES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
-export interface Post {
-  title: string;
-  text: string;
-  id?: number;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-
 })
-export class AppComponent implements OnInit {
-  posts: Array<Post> = [
-    {
-      title: 'I need to learn Angular Components',
-      text: 'And still learning...😁',
-      id: 1,
-    },
-    { title: 'Next block about', text: 'directives and something else', id: 2 },
-  ];
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      console.log('Timeout');
-      this.posts[0].title = 'New';
-    }, 5000);
-  }
-
-  updatePosts(post: Post): void {
-    this.posts.unshift(post);
-  }
-
-  removePost(id: number): void {
-    this.posts = this.posts.filter((p) => p.id !== id);
-  }
-}
+export class AppComponent {}
