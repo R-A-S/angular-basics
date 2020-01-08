6509950f8560d1aaa928e62c886028165f104ca0
R-A-S <r.a.s@live.ru>
2020-01-08 17:25:57 +0200

    custom Structural Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { StyleDirective } from './directives/style.directive';
+import { IfnotDirective } from './directives/ifnot.directive';
 
 @NgModule({
-  declarations: [AppComponent, StyleDirective],
+  declarations: [AppComponent, StyleDirective, IfnotDirective],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
