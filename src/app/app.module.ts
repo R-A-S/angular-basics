50c6722a276c53151b5b30c09d2d1812f9e23087
R-A-S <r.a.s@live.ru>
2020-01-08 11:28:25 +0200

    create directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { StyleDirective } from './directives/style.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, StyleDirective],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
