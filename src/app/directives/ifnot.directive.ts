6cef1f8f11f3c4ca986f484cf8a0b8055c6834e7
R-A-S <r.a.s@live.ru>
2020-01-08 17:26:28 +0200

    custom Structural Directives

---
+++ src/app/directives/ifnot.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appIfNot]',
+})
+export class IfnotDirective {
+  @Input('appIfNot') set ifNot(condition: boolean) {
+    if (!condition) {
+      // Show elements
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      // Hide elements
+      this.viewContainer.clear();
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) {}
+}
