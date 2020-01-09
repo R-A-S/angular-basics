6f1b60b82cf54e766f96d581d7c28c52107332f9
R-A-S <r.a.s@live.ru>
2020-01-09 17:07:09 +0200

    async validator

--- src/app/my.validators.ts
+++ src/app/my.validators.ts
@@ -1,10 +1,29 @@
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+interface RestrictedEmail {
+  [key: string]: boolean;
+}
 
 export class MyValidators {
-  static restrictedEmails(control: FormControl): { [key: string]: boolean } {
+  static restrictedEmails(control: FormControl): RestrictedEmail {
     if (['v@mail.ru', 'test@mail.ru'].includes(control.value)) {
       return { restrictedEmail: true };
     }
     return null;
+  }
+
+  static uniqEmail(
+    control: FormControl,
+  ): Promise<RestrictedEmail> | Observable<RestrictedEmail> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        if (control.value === 'async@mail.ru') {
+          resolve({ uniqEmail: true });
+        } else {
+          resolve(null);
+        }
+      }, 1000);
+    });
   }
 }
