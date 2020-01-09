c00e44fb7a62fd55e2843e67152bd0bccaae5de4
R-A-S <r.a.s@live.ru>
2020-01-09 12:21:27 +0200

    create service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AppCounterService } from './services/app-counter.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [AppCounterService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
