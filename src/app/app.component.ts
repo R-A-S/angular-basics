24d426475e194144ad6d19b5626eee98938bd5a9
R-A-S <r.a.s@live.ru>
2020-01-09 12:09:34 +0200

    CERVICES

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-
-export interface Post {
-  title: string;
-  text: string;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  p: Promise<string> = new Promise<string>((resolve) => {
-    setTimeout(() => resolve('resolved'), 2000);
-  });
-
-  date$: Observable<Date> = new Observable<Date>((obs) => {
-    setInterval(() => {
-      obs.next(new Date());
-    }, 1000);
-  });
-
-  date: Date;
-
-  ngOnInit(): void {
-    this.date$.subscribe((date) => {
-      this.date = date;
-    });
-  }
-}
+export class AppComponent {}
