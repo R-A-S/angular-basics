778301c7e1b6b18a9862f3d6fba165a348cfe6de
R-A-S <r.a.s@live.ru>
2020-01-09 15:31:22 +0200

    creating groups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,23 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      address: new FormGroup({
+        country: new FormControl('ua'),
+        city: new FormControl('', Validators.required),
+      }),
     });
   }
 
   submit(): void {
-    if (this.form.invalid){
+    if (!this.form.invalid) {
       console.log('Form submitted: ', this.form);
       const formData = { ...this.form.value };
 
       console.log('Form data: ', formData);
     }
-
   }
 }
