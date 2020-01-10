47b55498c693c80739e26e2cb6cf314bf1afe110
R-A-S <r.a.s@live.ru>
2020-01-10 12:20:18 +0200

    redirectTo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,26 @@ import { AboutComponent } from './about/about.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostComponent } from './post/post.component';
 import { AboutExtraComponent } from './about-extra/about-extra.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 // http://localhost:4200/ → HomeComponent
 // http://localhost:4200/about → AboutComponent
 //  → http://localhost:4200/about/extra → AboutExtraComponent
 // http://localhost:4200/posts → PostsComponent
 // http://localhost:4200/posts/:id → PostComponent
+// http://localhost:4200/error → ErrorPageComponent
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: 'about',
     component: AboutComponent,
-    children: [{ path: 'extra', component: AboutExtraComponent }]
+    children: [{ path: 'extra', component: AboutExtraComponent }],
   },
   { path: 'posts', component: PostsComponent },
   { path: 'posts/:id', component: PostComponent },
+  { path: 'error', component: ErrorPageComponent },
+  { path: '**', redirectTo: '/error' },
 ];
 
 @NgModule({
