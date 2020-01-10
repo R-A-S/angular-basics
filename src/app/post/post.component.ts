3151518705c57e61ce0d35c192db86fc77fd2944
R-A-S <r.a.s@live.ru>
2020-01-10 10:55:59 +0200

    parameter processing

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Post, PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {}
+export class PostComponent implements OnInit {
+  post: Post;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private postsService: PostsService,
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      // console.log('Params: ', params);
+      this.post = this.postsService.getById(parseInt(params.id, 10));
+    });
+  }
+
+  loadPost(): void {
+    this.router.navigate(['/posts', 44]);
+  }
+}
