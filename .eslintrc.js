e6dbc2751e0437496c41791f824213a42ba80f44
R-A-S <r.a.s@live.ru>
2020-01-10 17:44:34 +0200

    PWA

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['./node_modules/poetic/config/eslint/eslint-config.js'],
-  // Add custom rules here
-  rules: {
-    'import/prefer-default-export': 'off',
-  },
-};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ModalComponent } from './modal/modal.component';
 import { RefDirective } from './ref.directive';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, ModalComponent, RefDirective],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   entryComponents: [ModalComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+//TODO
+// ng add @angular/pwa
+// ng build --prod
+// npm i http-server -g
+// cd dist
+// cd <your project folder name>
+// http-server -p 8080
